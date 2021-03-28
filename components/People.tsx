c405466bbeabffa5c749c20667db47afaf58afdf
Aaron Adams <aadams99@gmail.com>
2021-03-28 17:46:26 -0500

    update

--- components/People.tsx
+++ components/People.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import useStore from '../lib/useStore.ts'
 
-const People = () => {
+export default function People() {
   const people = useStore(state => state.people);
 
   return (
     <div>
-      {/* <p>We have { people.length } people in our DB </p> */}
-      {/* <ul>
+      <p>We have { people.length } people in our DB </p>
+      <ul>
         {people.map(p => (
-          <li>{p}</li>
-        )}
-      </ul> */}
+          <li key='{p}'>{p}</li>
+      ))}
+      </ul>
     </div>
   )
 }
-
-export default People;
